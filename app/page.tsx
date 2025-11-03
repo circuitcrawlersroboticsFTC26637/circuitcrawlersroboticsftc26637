e279ce5f58297442fec4f4c124efdaf1a8964b3a
esha0281 <eshams28@outlook.com>
2025-11-03 00:47:46 -0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import Image from "next/image";
 import dynamic from 'next/dynamic';
 const FaultyTerminal = dynamic(() => import('./FaultyTerminal'), {
-  ssr: false,
+  ssr: true,
 });
 
 export default function Home() {
