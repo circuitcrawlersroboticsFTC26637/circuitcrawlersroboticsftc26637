f79d13c65217438c17da514de3a5052b2c3956f0
esha0281 <eshams28@outlook.com>
2025-11-03 01:20:33 -0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           priority
         /> */}
       {/* flex min-h-screen w-full max-w-3xl flex-col */}
-      <main className="items-center justify-center" style={{position: "absolute", top: "50%", left: "50%", width: "100%", height: "100%"}}>
+      <main className="items-center justify-center" style={{position: "absolute", width: "100%", height: "100%"}}>
         <Image
           className="dark:invert"
           src="/logo.svg"
