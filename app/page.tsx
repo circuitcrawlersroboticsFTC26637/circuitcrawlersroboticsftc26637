b3e21f8924c483130584b4d2b0e5e3c6e5a2d86c
esha0281 <eshams28@outlook.com>
2025-11-03 01:09:04 -0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,16 @@ export default function Home() {
         pageLoadAnimation={false}
         brightness={1}
       />
-    
+      {/* <Image
+          className="dark:invert"
+          src="/logo.svg"
+          alt="Robotics Team logo"
+          width={1000}
+          height={1000}
+          priority
+        /> */}
       {/* flex min-h-screen w-full max-w-3xl flex-col */}
-      {/* <main className="items-center justify-center py-32 px-16 bg-opacity-0 sm:items-start"> */}
+      {/* <main className="items-center justify-center py-32 px-16 bg- sm:items-start"> */}
         
         {/* <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
